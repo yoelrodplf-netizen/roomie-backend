5f4098fd0b6d63b121f511929ce095416d446fd5
Yoel Alejandro Rodriguez Palafox <yoelrodplf@gmail.com>
2025-11-20 14:20:34 -0600

    fix: corregir ruta de middleware auth

--- src/routes/profile.routes.js
+++ src/routes/profile.routes.js
@@ -2,8 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { updateProfile, getMyProfile, uploadSingle } = require('../controllers/profile.controller');
 const { likeProfile } = require('../controllers/like.controller');
-const auth = require('../middleware/auth');
-
+const auth = require('../middleware/auth.middleware');
 router.get('/me', auth, getMyProfile);
 router.put('/me', auth, uploadSingle, updateProfile);
 router.post('/like/:targetId', auth, likeProfile);
